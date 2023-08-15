c414f8e1a8a42ae897299d14b3dd126279d80b8f
Ayushi20-alt <107174083+Ayushi20-alt@users.noreply.github.com>
2023-08-16 01:19:40 +0530

    Final Commit

---
+++ app/src/main/java/com/example/codsoft_task2/CalculatorOperation.kt
@@ -0,0 +1,8 @@
+package com.example.codsoft_task2
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add: CalculatorOperation("+")
+    object Subtract: CalculatorOperation("-")
+    object Multiply: CalculatorOperation("x")
+    object Divide: CalculatorOperation("/")
+}
---
+++ app/src/main/java/com/example/codsoft_task2/CalculatorState.kt
@@ -0,0 +1,7 @@
+package com.example.codsoft_task2
+
+data class CalculatorState(
+    val number1: String = "",
+    val number2: String = "",
+    val operation: CalculatorOperation? = null
+)
